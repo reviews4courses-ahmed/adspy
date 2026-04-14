ec0f2bc898d222d916a0eef6f1de1f6358be300e
reviews4courses-ahmed <reviews4courses@gmail.com>
2026-04-15 00:08:29 +0400

    Create serp.js

---
+++ api/serp.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  const params = new URLSearchParams(req.query);
+
+  try {
+    const response = await fetch(`https://serpapi.com/search.json?${params}`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
